c614775b7005c8f49c439e55b146c95ef78676f1
Jumo0003 <Jumo0003@stud.ek.dk>
2026-02-27 14:03:25 +0100

    lille ekstra detalje

--- productlist.js
+++ productlist.js
@@ -17,9 +17,9 @@ function showProducts(products) {
           <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="Produkt 1" />
           <div class="tekst">
           <h3>${product.productdisplayname}</h3>
-           <p> ${product.gender}</p><p>${product.category}</p><p>${product.subcategory}</p>
+           <p> ${product.gender}</p>
+           <p>${product.category}</p>
         <p>Pris: ${product.price}</p>
-        <p> ${product.discount}</p>
         <p> ${product.brandname}</p>
         <a href="produkt.html">Køb</a>
 </div>
